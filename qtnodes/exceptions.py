acd3e989a0544eb534fd934c6b952bf470d03e52
Christoph Buelter <c.buelter@arcor.de>
2016-05-01 23:48:30 +0200

    Remove unused exception.

--- qtnodes/exceptions.py
+++ qtnodes/exceptions.py
@@ -11,7 +11,3 @@ class UnknownFlowError(QtNodesError):
 
 class KnobConnectionError(QtNodesError):
     """Something went wrong while trying to connect two Knobs."""
-
-
-class EdgeInvalidError(QtNodesError):
-    """The Edge connects things in a way that is wrong."""
