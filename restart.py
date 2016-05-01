11297de05f322c1f9bac50a58639bf488bc11a6e
Christoph Buelter <c.buelter@arcor.de>
2016-05-01 03:12:24 +0200

    Add more test code.

--- restart.py
+++ restart.py
@@ -689,12 +689,56 @@ class BigNode(Node):
         self.addKnob(OutputKnob(labelText="o9"))
 
 
+class Directory(Node):
+
+    def __init__(self, *args, **kwargs):
+        text = kwargs.pop("text")
+        super(Directory, self).__init__(*args, **kwargs)
+        self.addHeader(Header(node=self, text=text))
+        self.addKnob(InputKnob(labelText="parent"))
+        self.addKnob(OutputKnob(labelText="children"))
+
+
+class File(Node):
+
+    def __init__(self, *args, **kwargs):
+        text = kwargs.pop("text")
+        super(File, self).__init__(*args, **kwargs)
+        self.addHeader(Header(node=self, text=text))
+        self.addKnob(InputKnob(labelText="parent"))
+
+
 def test():
     app = QtGui.QApplication([])
 
     graph = NodeGraphWidget()
     graph.setGeometry(100, 100, 800, 600)
     graph.show()
+
+    # d1 = Directory(scene=graph.scene, text="root")
+    # d2 = Directory(scene=graph.scene, text="opt")
+    # d3 = Directory(scene=graph.scene, text="rengo")
+    # d4 = Directory(scene=graph.scene, text=".venvs")
+    # d5 = Directory(scene=graph.scene, text="rengo")
+    # d6 = Directory(scene=graph.scene, text="rengo01")
+    # d7 = Directory(scene=graph.scene, text="bin")
+    # d8 = File(scene=graph.scene, text="activate")
+
+    # d2.moveBy(100, 0)
+    # d3.moveBy(200, 0)
+    # d4.moveBy(200, 70)
+    # d5.moveBy(300, 70)
+    # d6.moveBy(300, 0)
+    # d7.moveBy(400, 70)
+    # d8.moveBy(500, 70)
+
+    # d1.knob("children").connectTo(d2.knob("parent"))
+    # d2.knob("children").connectTo(d3.knob("parent"))
+    # d2.knob("children").connectTo(d4.knob("parent"))
+    # d4.knob("children").connectTo(d5.knob("parent"))
+    # d3.knob("children").connectTo(d6.knob("parent"))
+    # d5.knob("children").connectTo(d7.knob("parent"))
+    # d7.knob("children").connectTo(d8.knob("parent"))
 
     nodeInt1 = Integer(scene=graph.scene)
     nodeInt2 = Integer(scene=graph.scene)
@@ -716,9 +760,9 @@ def test():
     nodeMult.knob("value").connectTo(nodeBig.knob("i4"))
     nodeMult.knob("value").connectTo(nodeBig.knob("i5"))
     nodeMult.knob("value").connectTo(nodeBig.knob("i6"))
-    # nodeMult.knob("value").connectTo(nodeBig.knob("i7"))
-    # nodeMult.knob("value").connectTo(nodeBig.knob("i8"))
-    # nodeMult.knob("value").connectTo(nodeBig.knob("i9"))
+    nodeMult.knob("value").connectTo(nodeBig.knob("i7"))
+    nodeMult.knob("value").connectTo(nodeBig.knob("i8"))
+    nodeMult.knob("value").connectTo(nodeBig.knob("i9"))
 
     nodeBig.knob("o1").connectTo(nodeOut.knob("output"))
     nodeBig.knob("o2").connectTo(nodeOut.knob("output"))
@@ -726,9 +770,9 @@ def test():
     nodeBig.knob("o4").connectTo(nodeOut.knob("output"))
     nodeBig.knob("o5").connectTo(nodeOut.knob("output"))
     nodeBig.knob("o6").connectTo(nodeOut.knob("output"))
-    # nodeBig.knob("o7").connectTo(nodeOut.knob("output"))
-    # nodeBig.knob("o8").connectTo(nodeOut.knob("output"))
-    # nodeBig.knob("o9").connectTo(nodeOut.knob("output"))
+    nodeBig.knob("o7").connectTo(nodeOut.knob("output"))
+    nodeBig.knob("o8").connectTo(nodeOut.knob("output"))
+    nodeBig.knob("o9").connectTo(nodeOut.knob("output"))
 
     app.exec_()
 
@@ -740,8 +784,6 @@ if __name__ == '__main__':
 """
 todos
 
-- get rid of the SceneEventHandler somehow and encapsulate the logic
-  in each respective class
 - that should hopefully enable us to use RubberBandDrag together with
   creating edges from a knob
 - attach data to nodes and let them modify it
